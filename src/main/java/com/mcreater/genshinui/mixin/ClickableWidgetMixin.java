23a6b3ed0888fd929616b3727be0edd9267654e9
Jack253-png <guoxiuchen20170402@163.com>
2023-07-08 21:33:00 +0800

    updated on 2023.7.8 (0.0.1 version end)

---
+++ src/main/java/com/mcreater/genshinui/mixin/ClickableWidgetMixin.java
@@ -0,0 +1,60 @@
+package com.mcreater.genshinui.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.minecraft.client.gui.widget.ClickableWidget.WIDGETS_TEXTURE;
+
+@Mixin(value = {ClickableWidget.class}, priority = Integer.MAX_VALUE)
+public abstract class ClickableWidgetMixin extends DrawableHelper {
+    @Shadow protected abstract int getYImage(boolean hovered);
+
+    @Shadow protected float alpha;
+
+    @Shadow public abstract boolean isHovered();
+
+    @Shadow protected int width;
+
+    @Shadow protected int height;
+
+    @Shadow public abstract Text getMessage();
+
+    @Shadow public int x;
+
+    @Shadow public int y;
+
+    @Shadow public boolean active;
+
+    @Shadow protected abstract void renderBackground(MatrixStack matrices, MinecraftClient client, int mouseX, int mouseY);
+
+    @Inject(at = @At("HEAD"), method = "renderButton")
+    public void onRenderButton(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        MinecraftClient minecraftClient = MinecraftClient.getInstance();
+        TextRenderer textRenderer = minecraftClient.textRenderer;
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, WIDGETS_TEXTURE);
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, this.alpha);
+
+        int i = this.getYImage(this.isHovered());
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.enableDepthTest();
+        this.drawTexture(matrices, this.x, this.y, 0, 46 + i * 20, this.width / 2, this.height);
+        this.drawTexture(matrices, this.x + this.width / 2, this.y, 200 - this.width / 2, 46 + i * 20, this.width / 2, this.height);
+        this.renderBackground(matrices, minecraftClient, mouseX, mouseY);
+        int j = this.active ? 0x323232 : 0x878787;
+        ClickableWidget.drawCenteredText(matrices, textRenderer, this.getMessage(), this.x + this.width / 2, this.y + (this.height - 8) / 2, j | MathHelper.ceil(this.alpha * 255.0f) << 24);
+    }
+}
--- src/main/java/com/mcreater/genshinui/screens/widget/GenshinNormalButtonWidget.java
+++ src/main/java/com/mcreater/genshinui/screens/widget/GenshinNormalButtonWidget.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class GenshinNormalButtonWidget extends ButtonWidget {
+    // 212 213 204
     public GenshinNormalButtonWidget(int x, int y, int width, int height, Text message, PressAction onPress) {
         super(x, y, width, height, message, onPress);
     }
