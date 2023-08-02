4813df281c66c442d9fd0d122d7dd7300d0f45e1
Jack253-png <guoxiuchen20170402@163.com>
2023-08-02 16:46:49 +0800

    updated on 2023.8.2

--- src/main/java/com/mcreater/genshinui/elements/GenshinBlocks.java
+++ src/main/java/com/mcreater/genshinui/elements/GenshinBlocks.java
@@ -1,21 +1,19 @@
 package com.mcreater.genshinui.elements;
 
 import com.mcreater.genshinui.elements.skyisland.block.GenshinSkyIslandPathBlock;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import static com.mcreater.genshinui.GenshinUIClient.MOD_ID;
 
 public class GenshinBlocks {
-    public static ItemGroup SKY_ISLAND_GROUP;
     public static GenshinSkyIslandPathBlock GENSHIN_SKY_ISLAND_PATH_BLOCK;
+    public static Item GENSHIN_SKY_ISLAND_PATH_BLOCK_ITEM;
     public static void register() {
         GENSHIN_SKY_ISLAND_PATH_BLOCK = Registry.register(
                 Registry.BLOCK,
@@ -23,14 +21,10 @@ public class GenshinBlocks {
                 new GenshinSkyIslandPathBlock(FabricBlockSettings.of(Material.BARRIER).strength(1.0F).nonOpaque())
         );
 
-        SKY_ISLAND_GROUP = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "sky_island"))
-                .icon(() -> new ItemStack(GENSHIN_SKY_ISLAND_PATH_BLOCK))
-                .build();
-
-        Registry.register(
+        GENSHIN_SKY_ISLAND_PATH_BLOCK_ITEM = Registry.register(
                 Registry.ITEM,
                 new Identifier(MOD_ID, "genshin_sky_island_path_block"),
-                new BlockItem(GENSHIN_SKY_ISLAND_PATH_BLOCK, new FabricItemSettings().group(SKY_ISLAND_GROUP))
+                new BlockItem(GENSHIN_SKY_ISLAND_PATH_BLOCK, new FabricItemSettings())
         );
     }
 }
